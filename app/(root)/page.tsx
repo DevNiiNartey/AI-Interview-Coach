dcf27431263b9c2fa9caac8aa84371700bacfd84
Nii Nartey <nii.tetteh@amalitech.com>
2025-06-03 11:50:27 +0000

    Update `RootLayout` and homepage with navigation, branding, and CTA sections
    Added a branded navigation bar with logo and title to `RootLayout`. Enhanced the homepage with new sections, including a CTA for interviews, and placeholders for user interview data. Integrated `Link` and `Image` components for better structure and visuals.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,36 @@
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Page() {
-    return <div>HomePage</div>
+    return (
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Get Interview-Ready with AI-Powered Practice and Feedback</h2>
+                    <p>Practice on real interview questions and get instance feedback</p>
+                    <Button asChild className="btn-primary max-sm:w-full">
+                        <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
+
+                <Image src="/robot.png" alt="robot" width={400} height={400} className="max-sm:hidden"/>
+
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+                <div className="interview sections">
+                    <p>You haven&apos;t taken interviews yet</p>
+                </div>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take your Interview</h2>
+                <div className="inteviews-section">
+                    <p>There are no interviews available</p>
+                </div>
+            </section>
+        </>
+    )
 }
