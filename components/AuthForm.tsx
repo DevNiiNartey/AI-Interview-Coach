4ef7599b7b03c50d99f294c73efaa959abe8b688
Nii Nartey <nii.tetteh@amalitech.com>
2025-06-05 08:58:22 +0000

    Add server-side user creation with Firebase in sign-up flow
    Implemented `signUp` server-side action for user creation in Firestore. Enhanced `AuthForm` to handle user registration via Firebase client and server-side integration, with proper error handling and toasts for feedback.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,6 +11,9 @@ import Image from "next/image";
 import {toast} from "sonner";
 import FormField from "@/components/FormField";
 import {useRouter} from "next/navigation";
+import {createUserWithEmailAndPassword} from "@firebase/auth";
+import {auth} from "@/firebase/client";
+import {signUp} from "@/lib/actions/auth.action";
 
 
 const authFormSchema = (type: FormType) => {
@@ -38,9 +41,25 @@ const AuthForm = ({type}: { type: FormType }) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if (type === 'sign-up') {
+                const {name, email, password} = values;
+
+                const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+
+                const result = await signUp({
+                    uid: userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password
+                });
+
+                if (!result?.success) {
+                    toast.error(result?.message)
+                    return
+                }
+
                 toast.success("Account created successfully. Please sign in.")
                 router.push('/sign-in')
             } else {
---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import {db} from "@/firebase/admin";
+
+export async function signUp(params: SignUpParams) {
+    const {uid, name, email} = params;
+    try {
+        const userRecord = await db.collection('users').doc(uid).get();
+
+        if (userRecord.exists) {
+            return {success: false, message: "User already exists, Please sign in instead"}
+        }
+
+        await db.collection("users").doc(uid).set({
+            name,
+            email,
+        })
+
+    } catch (e: any) {
+        console.error("Error creating user: ", e)
+        if (e.code === "auth/email-already-in-use") {
+            return {
+                success: false,
+                message: "Email already in use"
+            }
+        }
+        return {
+            success: false,
+            message: e.message || "Failed to create user account"
+        }
+    }
+}
